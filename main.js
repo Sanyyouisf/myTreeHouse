81183460c868da49477e73819c8defa18195812c
Sanyyouisf <sanymamdoh@yahoo.com>
2017-03-08 20:35:14 -0600

    finish

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ function makeDom(xhrData){
 	// console.log("my xhrData",xhrData);   
     var TreeHouseString ;
     var currentTreeHouse;
-
     for(var x = 0; x < xhrData.badges.length; x++) {
         currentTreeHouse = xhrData.badges[x];
         TreeHouseString += `<div class="col-sm-6 col-md-4">`;
@@ -13,32 +12,19 @@ function makeDom(xhrData){
         TreeHouseString += `<img src=${xhrData.badges[x].icon_url} >`;
         TreeHouseString += `<p> ${xhrData.badges[x].name} </p>`;
         TreeHouseString += `</div></div>`;
-
     }
-
     TreeHouseContainer.innerHTML += TreeHouseString;
-    // console.log("myname is  : " , xhrData.name);
 }
 
 
 function executeThisCodeAfterFileLoaded(){
-	// console.log("Data Return :  ", Date.now());
-
     var data = JSON.parse(this.responseText);
     makeDom(data);
-
     console.log("it works!", data);
-    // makeDom(data);
-}
-function executeThisCodeAfterFileFails(){
-   
 }
 
 myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
-myRequest.addEventListener("error", executeThisCodeAfterFileFails);
 
-//now we do something to the things || 2 things... 
-//GET/POST/PUT/DELETE..etc.. SECOND IS WHERE DO I GET IT FROM?
 myRequest.open("GET", "https://teamtreehouse.com/sanyyousif.json");
 
 myRequest.send();
